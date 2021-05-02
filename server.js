1b080c0daaf0dcdff3dd3b7eec2552668c3e285c
Olivia Faraone <oliviafaraone1@MacBook-Pro-10.local>
2021-05-02 14:37:48 -0700

    troubleshoot insomnia

--- server.js
+++ server.js
@@ -4,15 +4,13 @@ const mongoose = require('mongoose');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//const { User  } = require('./models');
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1/social-network', {
   useFindAndModify: false,
   useNewUrlParser: true,
   useUnifiedTopology: true
